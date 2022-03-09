cd363db77e0a0b8bbcd2f2544b93de2a8105ea22
nevik <bartlamkevin@gmail.com>
2022-03-09 18:12:33 -0500

    done

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("Let's get this party started!");
+
+function getGifs(results){
+    let numResults = results.data.length;
+    if (numResults){
+        let ranIdx = Math.floor(Math.random()*numResults);
+        let newGif = $(`<img src = "${results.data[ranIdx].images.original.url}">`);
+        let test = $("<div>");
+        test.append(newGif)
+        $("#gifs").append(newGif);
+    }
+}
+
+$("form").on("submit", async function(evt){
+    evt.preventDefault();
+
+    let search = $("#search").val();
+    $("#search").val("");
+
+    const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+        params: {
+            q: search,
+            api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        }
+    });
+    getGifs(response.data);
+});
+
+$("#delete").on("click", function(){
+    $("#gifs").empty();
+});
+
